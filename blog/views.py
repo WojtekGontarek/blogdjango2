46742088b9c9b4938dbebb97022a0044e2746177
C04H03 <C04H03@teb.edu.pl>
2024-03-22 13:21:48 +0100

    dfsadsfds

--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,21 @@
 from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView
 
 from blog.models import Post
 
 
 # Create your views here.
-def post_list(request):
-    posts = Post.published.all()
-    return render(request, "blog/post/list.html", {"posts": posts})
+# def post_list(request):
+#     posts = Post.published.all()
+#     return render(request, "blog/post/list.html", {"posts": posts})
+
+class PostListView(ListView):
+
+     queryset = Post.published.all()
+     context_object_name = 'posts'
+     paginate_by = 2
+     template_name = 'blog/post/list.html'
 
 def post_detail(request, year, month, day, slug):
     post = get_object_or_404(Post, slug=slug, status="published",
