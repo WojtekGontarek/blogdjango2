1d966e40b026c93b1813544ae7da9362f0ecf3d2
C04H03 <C04H03@teb.edu.pl>
2024-04-12 13:18:09 +0200

    dfsadsfds

--- blog/models.py
+++ blog/models.py
@@ -8,6 +8,7 @@ class PublishedManager(models.Manager):
     def get_queryset(self):
         return super(PublishedManager, self).get_queryset().filter(status='published')
 
+
 class Post(models.Model):
     # menadzery obiketow
     objects = models.Manager()
@@ -40,4 +41,16 @@ class Post(models.Model):
         return reverse('blog:post_detail', args=[self.publish.year, self.publish.month, self.publish.day, self.slug])
 
 
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True)
+    class Meta:
+        ordering = ('created',)
 
+    def __str__(self):
+        return f'Komentarz dodany przez {self.name} dla posta {self.post}'
--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView
 
+from blog.forms import CommentForm
 from blog.models import Post
 
 
@@ -17,9 +18,20 @@ class PostListView(ListView):
      paginate_by = 2
      template_name = 'blog/post/list.html'
 
+
 def post_detail(request, year, month, day, slug):
     post = get_object_or_404(Post, slug=slug, status="published",
                              publish__year=year,
                              publish__month=month,
                              publish__day=day)
-    return render(request, 'blog/post/detail.html', {'post':post})
+    comments = post.comments.filter(active=True)
+    if request.method == 'POST':
+          comment_form = CommentForm(request.POST)
+          if comment_form.is_valid():
+               comment = comment_form.save(commit=False)
+               comment.post = post
+               comment.save()
+
+    comment_form = CommentForm()
+
+    return render(request, 'blog/post/detail.html', {'post': post, 'comments': comments, 'comment_form': comment_form})
